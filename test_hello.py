d18c3ab3f76dcf17e603b0cb064e6044efebdc53
asdf <asdf>
2016-12-03 22:45:12 +0000

    Web app

---
+++ test_hello.py
@@ -0,0 +1,19 @@
+import unittest
+from app import app
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_hello(self):
+        test_url = '/hello/bob'
+        expected_response = "Hello, bob!"
+
+        actual_response = self.client.get(test_url).data
+
+        # Compare the actual and expected responses
+	assert expected_response == actual_response
+
+if __name__ == "__main__":
+    unittest.main()
